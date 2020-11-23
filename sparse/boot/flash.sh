fc1ef0e8a43d4bea774ae125ee1fe07994803ef1
Adam Pigg <adam@piggz.co.uk>
2020-11-23 16:09:53 +0000

    Add simple readme and splash flash

--- sparse/boot/flash.sh
+++ sparse/boot/flash.sh
@@ -1,3 +1,9 @@
+#README
+#Use this script to flash the device.  The device must
+#be in "fastboot" (or bootloader) mode.
+#It may be nescessary to run this script as root.
+#Usage: sh flash.sh
+
 #Kernel and init
 fastboot flash boot_a hybris-boot.img
 
@@ -9,3 +15,6 @@ fastboot flash vendor_b vendor.img001
 
 #system_b is used for the factory restore images
 fastboot flash system_b fimage.img001
+
+#Splash Image
+fastboot flash splash splash.img
